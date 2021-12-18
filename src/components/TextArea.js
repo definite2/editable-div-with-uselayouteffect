b7743ef7bba50ddaa79d9f680112a412d29ade76
definite2 <edefne878@gmail.com>
2021-12-18 09:44:40 +0300

    this is base of textarea component

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,27 +1,12 @@
 import React, { useRef } from "react";
-import { getTextSegments } from "../utils/getTextSegments";
+
 const TextArea = ({ highlighted, ...rest }) => {
   const editorRef = useRef();
-  //simple function that highlights some of words while editing
-  //it first gets text segments array
-  const highlightText = (text) => {
-    const words = text.split(/(\s+)/);
-    const output = words.map((word) => {
-      if (highlighted.includes(word)) {
-        return `<span style="text-decoration: line-through; color:red">${word}</span>`;
-      } else {
-        return word;
-      }
-    });
-    return output.join("");
-  };
 
   //onInputChnage callback function
   const handleInputChange = (_) => {
     const editor = editorRef.current;
-    const textSegments = getTextSegments(editor);
-    const textContent = textSegments.map(({ text }) => text).join("");
-    highlightText(textContent);
+    console.log(editor.innerText);
   };
 
   return (
